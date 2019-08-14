11a45ca5a96c40d103eb9fced92d6b06f7f60ecc
张一鹏 <yipeng.zhang@zymobi.com>
2019-08-14 20:35:01 +0800

    适配4.3引擎，完善Demo的注释和说明

--- README.md
+++ README.md
@@ -1,28 +1,69 @@
-# AppcanPluginDemo-Gradle
+## AppCanPluginDemo-Gradle
+
 AppCan Android 插件demo
 
-####Gradle打包的优势
+### Gradle打包的优势
 
 * **插件工程简洁:** 插件工程引擎部分内容全部在引擎的`aar`文件里面,插件工程只包含插件部分的内容
 * **插件包生成简单:** Gradle打包支持`aar`文件,可以把插件包除`info.xml`的部分全部打包在`aar`文件里面
 * **调试插件简单:** 前端使用多个插件时,调试只需要把插件包的`aar`文件放入`libs`目录即可
 * **支持R.XX.XX方式调用资源:** 用`aar`生成插件包时,可以直接使用`R.id.xxx`
 
-####目前已知的问题
+#### 目前已知的问题
 
 * 由于要启动的`Activity`在引擎的`aar`里面,IDE目前不能识别,直接run插件工程会不能启动应用,需要手动点击应用或者在IDE指定启动的`Activity`  
 * `aar`只能在Gradle编译环境下使用.建议不支持的企业版打包服务升级.  
 * 由于打包服务器暂时不能连外网,远程依赖不能用,只能把依赖包(`aar`,`jar`)下载到本地.╮(╯▽╰)╭   
 * `plugin.xml`打包时不能自动merge，需要手动复制到插件包目录下。   
 
-###aar插件制作步骤(插件中有用到R.xx.xx)
+### 插件开发的Gradle打包配置
 
-1、 参考Demo工程编写调试好插件  
-2、 将`build.gradle`中的`apply plugin: 'com.android.application'`修改成`apply plugin: 'com.android.library'`  
-3、 make插件工程，拷贝`build->outputs->aar->***.aar`至插件包`jar`目录下，重命名为插件名称。  
-4、 用压缩工具打开aar，删除`assets/widget`目录,删除`AndroidManifest.xml`的`minSdkVersion`，删除`plugin.xml`文件  
-5、 复制`plugin.xml`文件到插件包目录,填写`info.xml`,制作插件包。  
-6、 将`build.gradle`中的`apply plugin: 'com.android.library'`修改成`apply plugin: 'com.android.application'`  
+#### Gradle版本配置
+
+1. gradle-wrapper.properties中的配置，在当前Demo中推荐使用4.1版本，即：
+
+```properties
+    distributionUrl=https\://services.gradle.org/distributions/gradle-4.1-all.zip
+```
+
+2. build.gradle中的Android编译用的gradle插件，在当前Demo中推荐使用3.0.1，即：
+
+```groovy
+    classpath 'com.android.tools.build:gradle:3.0.1'
+```
+
+3. 注意：以上版本仅供参考，代表已经测试并适配过。升级版本也不是不可以，但是如果有报错的话，可以反馈给官方，或者提issue，或者自己解决。解决不了的，就用推荐版本。
+
+#### 依赖最新版AppCan引擎进行测试
+
+> 由于compile关键词已经废弃，现在AppCanDemo中统一使用implementation
+
+1. 依赖指定版本的引擎（4.3+的引擎适配了Android9.0+）
+
+```groovy
+    implementation 'org.appcan:engine:4.3.23'
+```
+
+2. 依赖最新版本的引擎
+
+```groovy
+    implementation 'org.appcan:engine:latest.release'
+```
+
+3. 纠正一个之前文档中的错误：+号不代表最新版本，而是本地已经下载过的任意版本，不保险，不推荐使用。
+
+```groovy
+    implementation 'org.appcan:engine:+' // 不推荐使用此写法
+```
+
+### aar插件制作步骤(插件中有用到R.xx.xx)
+
+1. 参考Demo工程编写调试好插件  
+2. 将`build.gradle`中的`apply plugin: 'com.android.application'`修改成`apply plugin: 'com.android.library'`  
+3. make插件工程，拷贝`build->outputs->aar->***.aar`至插件包`jar`目录下，重命名为插件名称。  
+4. 用压缩工具打开aar，删除`assets/widget`目录,删除`AndroidManifest.xml`的`minSdkVersion`，删除`plugin.xml`文件  
+5. 复制`plugin.xml`文件到插件包目录,填写`info.xml`,制作插件包。  
+6. 将`build.gradle`中的`apply plugin: 'com.android.library'`修改成`apply plugin: 'com.android.application'`  
 
 如果是封装第三方SDK，需要通过config.xml配置appkey，插件包下面放一份`AndroidManifest.xml`，里面只包含需要替换的部分，**同时删掉aar里面对应的内容**，可以参考[融云插件](https://github.com/android-plugin/uexRongCloud)
 
@@ -55,3 +96,4 @@ AppCan Android 插件demo
 **以uexQQ为例：**（gif 1.7M）
 
  ![buildPlugin](img/buildPlugin.gif)
+ 
--- uexDemo/src/com/test/EUExDemo.java
+++ uexDemo/src/com/test/EUExDemo.java
@@ -5,22 +5,25 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.zywx.wbpalmstar.base.BDebug;
 import org.zywx.wbpalmstar.engine.DataHelper;
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
 
 public class EUExDemo extends EUExBase {
 
-    int mFuncActivityCallback =-1;
-
-    static final int mMyActivityRequestCode = 10000;
     private static final String TAG = "uexDemo";
+    
+    private int mFuncActivityCallback =-1;
+    private static final int mMyActivityRequestCode = 10000;
+
     private static final String CALLBACK_ON_VIEW_BUTTON_CLICK = "uexDemo.onViewButtonClick";
     private static final String CALLBACK_ON_FRAGMENT_BUTTON_CLICK = "uexDemo.onFragmentButtonClick";
 
@@ -31,11 +34,88 @@ public class EUExDemo extends EUExBase {
     private DemoFragment mAddFragmentView;
     private ViewDataVO mAddFragmentData;
 
-
-
     public EUExDemo(Context context, EBrowserView view) {
         super(context, view);
     }
+
+    // ============= 分割线 ==============
+    // 以下为生命周期事件监听方法，按需声明，不需要的话就不用声明。
+
+    /**
+     * 用于监听Application的onCreate事件，需要的写好这个方法用来接收引擎的通知。
+     * 利用此方法可以解决绝大部分需要自定义Application的情况（AppCan插件中不允许自定义Application）
+     *
+     * @param context ApplicationContext上下文实例
+     */
+    public static void onApplicationCreate(Context context) {
+        BDebug.i(TAG, TAG + " onApplicationCreate");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onCreate
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityCreate(Context context) {
+        BDebug.i(TAG, TAG + " onActivityCreate");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onStart
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityStart(Context context) {
+        BDebug.i(TAG, TAG + " onActivityStart");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onRestart
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityReStart(Context context) {
+        BDebug.i(TAG, TAG + " onActivityReStart");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onResume
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityResume(Context context) {
+        BDebug.i(TAG, TAG + " onActivityResume");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onPause
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityPause(Context context) {
+        BDebug.i(TAG, TAG + " onActivityPause");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onStop
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityStop(Context context) {
+        BDebug.i(TAG, TAG + " onActivityStop");
+    }
+
+    /**
+     * 对应加载插件的引擎Activity的生命周期方法onDestroy
+     *
+     * @param context 加载插件的引擎ActivityContext实例
+     */
+    public static void onActivityDestroy(Context context) {
+        BDebug.i(TAG, TAG + " onActivityDestroy");
+    }
+
+    // 以上为生命周期事件监听方法，按需声明，不需要的话就不用声明。
+    // ============= 分割线 ==============
 
     public void test_addView(String[] parm) {
         if (parm.length < 1) {
@@ -48,6 +128,7 @@ public class EUExDemo extends EUExBase {
         if (mAddView != null){
             test_removeView(null);
         }
+        // mContext变量是继承自父类的上下文，此Context实际上是加载本插件入口类对象的引擎Activity（4.0引擎中为EBrowserActivity的实例）
         mAddView = new DemoView(mContext, "我是一个添加的view的text",
                 "view button", onViewButtonClick);
         if (mAddViewData.isScrollWithWebView()){
@@ -82,6 +163,7 @@ public class EUExDemo extends EUExBase {
     // this case start a Activity: HelloAppCanNativeActivity
     public void test_startActivityForResult(String[] parm) {
         if (parm.length>0) {
+            // save the callbackId so you can callback to Javascript with it.
             mFuncActivityCallback = Integer.parseInt(parm[0]);
         }
         Intent intent = new Intent();
@@ -106,7 +188,11 @@ public class EUExDemo extends EUExBase {
                 m_v = (Vibrator) mContext
                         .getSystemService(Service.VIBRATOR_SERVICE);
             }
-            m_v.vibrate((int)inMilliseconds);
+            if (m_v != null) {
+                m_v.vibrate((int)inMilliseconds);
+            } else {
+                Toast.makeText(mContext, "Error: Vibrator is null", Toast.LENGTH_SHORT).show();
+            }
         } catch (SecurityException e) {
             Toast.makeText(mContext, "未配置震动权限或参数错误!!", Toast.LENGTH_LONG)
                     .show();
@@ -174,9 +260,29 @@ public class EUExDemo extends EUExBase {
         }
     }
 
+    /**
+     * 用于执行JS回调方法
+     *
+     * @param methodName 回调方法名
+     * @param jsonData 回调json数据
+     */
     private void callBackPluginJs(String methodName, String jsonData){
         String js = SCRIPT_HEADER + "if(" + methodName + "){"
                 + methodName + "('" + jsonData + "');}";
+        onCallback(js);
+    }
+
+    /**
+     * 用于执行JS回调方法（数据为JSON object）
+     * 推荐此方式，防止极个别情况下，回调数据中存在异常字符导致JS处理错误 by yipeng
+     *
+     * @param methodName 回调方法名
+     * @param jsonData 回调json数据
+     */
+    private void callBackPluginJsWithJsonObject(String methodName, String jsonData){
+        String js = SCRIPT_HEADER + "if(" + methodName + "){"
+                + methodName + "(" + jsonData + ");}";
+        // 是否看出与上面方法的区别？传参的括号内少了单引号，这样可以让JS收到回调时拿到的就是一个JS的object，而无需使用JSON.parse
         onCallback(js);
     }
 
@@ -205,17 +311,17 @@ public class EUExDemo extends EUExBase {
     }
 
     public interface OnButtonClick{
-        public void onButtonClick();
+        void onButtonClick();
     }
 
-    OnButtonClick onViewButtonClick = new OnButtonClick() {
+    private OnButtonClick onViewButtonClick = new OnButtonClick() {
         @Override
         public void onButtonClick() {
             callBackPluginJs(CALLBACK_ON_VIEW_BUTTON_CLICK, "");
         }
     };
 
-    OnButtonClick onFragmentButtonClick = new OnButtonClick() {
+    private OnButtonClick onFragmentButtonClick = new OnButtonClick() {
         @Override
         public void onButtonClick() {
             callBackPluginJs(CALLBACK_ON_FRAGMENT_BUTTON_CLICK, "");
